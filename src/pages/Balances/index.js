6b3b91056c8a080f0fbcb96e1da158f53be3b517
mk <mkdodos@gmail.com>
2023-03-02 16:35:14 +0800

    filter

--- src/pages/Balances/index.js
+++ src/pages/Balances/index.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { Container } from 'semantic-ui-react';
+import { Container, Input } from 'semantic-ui-react';
 import TableList from './components/TableList';
 import { db } from '../../utils/firebase';
 
@@ -9,26 +9,44 @@ export default function Index() {
 
   // 資料陣列
   const [rows, setRows] = useState([]);
+
+  // 篩選用資料陣列
+  let [rowsFilter, setRowsFilter] = useState([]);
+
   // 載入中
   const [loading, setLoading] = useState(false);
 
   // 筆數
   const [rowsCount, setRowsCount] = useState(20);
 
-
   // 取得資料
   useEffect(() => {
-    dbCol.orderBy('date','desc').limit(rowsCount).get().then((snapshot) => {
-      const data = snapshot.docs.map((doc) => {
-        return { ...doc.data(), id: doc.id };
+    dbCol
+      .orderBy('date', 'desc')
+      .limit(rowsCount)
+      .get()
+      .then((snapshot) => {
+        let data = snapshot.docs.map((doc) => {
+          return { ...doc.data(), id: doc.id };
+        });       
+        setRows(data);
+        setRowsFilter(data);
       });
-
-      setRows(data);
-    });
   }, []);
+
+  const filterData = (e) => {
+    // 篩選資料
+    const keyword = e.target.value;
+    rowsFilter = rowsFilter.filter((row) =>
+      row.title.toLowerCase().includes(keyword)
+    );
+    setRows(rowsFilter);
+  };
 
   return (
     <Container>
+      <Input onChange={filterData} />
+
       <TableList loading={loading} rows={rows} />
     </Container>
   );
