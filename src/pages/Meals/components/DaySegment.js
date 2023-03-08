cbe1fa80b8333a32d9c76b34f2d720823a6d875e
mk <mkdodos@gmail.com>
2023-03-08 08:29:03 +0800

    ymd

--- src/pages/Meals/components/DaySegment.js
+++ src/pages/Meals/components/DaySegment.js
@@ -5,43 +5,60 @@ export default function DaySegment({ rows }) {
   const [daysData, setDaysData] = useState([]);
   useEffect(() => {
     console.log(rows);
-    let newData = rows.filter((row) => row.date == '2023-02-05');
-
+    const ym = '2023-02-';
     const arrayData = [];
-    arrayData.push(newData)
+    for (let i = 1; i <= 31; i++) {
+      let ymd = '';
+      if (i < 10) {
+        ymd = ym + '0' + i;
+        // console.log(ym+'0'+i);
+      } else {
+        ymd = ym + i;
+        // console.log(ym + i);
+      }
 
-    newData = rows.filter((row) => row.date == '2023-02-07');
-    arrayData.push(newData)
+      let newData = rows.filter((row) => row.date == ymd);
+
+      console.log(newData);
+      if (newData.length > 0) arrayData.push(newData);
+    }
+
+    // newData = rows.filter((row) => row.date == '2023-02-07');
+    // arrayData.push(newData)
     setDaysData(arrayData);
-  }, []);
+  }, [rows]);
   return (
     <div>
-      <Segment>
-        <Table basic="very" celled collapsing unstackable>
-          <Table.Header>
-            <Table.Row>
-              <Table.HeaderCell>D</Table.HeaderCell>
-              <Table.HeaderCell>姓名</Table.HeaderCell>
-              <Table.HeaderCell>金額</Table.HeaderCell>
-            </Table.Row>
-          </Table.Header>
-          <Table.Body>
-            {daysData[0].map((row) => {
-              return (
-                <Table.Row key={row.id}>
-                  <Table.Cell>{row.date}</Table.Cell>
-                  <Table.Cell>
-                    <Header as="h4" image>
-                      <Header.Content>{row.name}</Header.Content>
-                    </Header>
-                  </Table.Cell>
-                  <Table.Cell>{row.amt}</Table.Cell>
+      {daysData.map((day) => {
+        return (
+          <Segment>
+            <Table basic="very" celled collapsing unstackable>
+              <Table.Header>
+                <Table.Row>
+                  <Table.HeaderCell>D</Table.HeaderCell>
+                  <Table.HeaderCell>姓名</Table.HeaderCell>
+                  <Table.HeaderCell>金額</Table.HeaderCell>
                 </Table.Row>
-              );
-            })}
-          </Table.Body>
-        </Table>
-      </Segment>
+              </Table.Header>
+              <Table.Body>
+                {day.map((row) => {
+                  return (
+                    <Table.Row key={row.id}>
+                      <Table.Cell>{row.date}</Table.Cell>
+                      <Table.Cell>
+                        <Header as="h4" image>
+                          <Header.Content>{row.name}</Header.Content>
+                        </Header>
+                      </Table.Cell>
+                      <Table.Cell>{row.amt}</Table.Cell>
+                    </Table.Row>
+                  );
+                })}
+              </Table.Body>
+            </Table>
+          </Segment>
+        );
+      })}
     </div>
   );
 }
