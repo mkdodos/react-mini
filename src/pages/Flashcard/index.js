be640c01556115c79090a3b412cec4decb21157d
mk <mkdodos@gmail.com>
2023-04-05 12:06:08 +0800

    abc

--- src/pages/Flashcard/index.js
+++ src/pages/Flashcard/index.js
@@ -1,43 +1,23 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { Container } from 'semantic-ui-react';
-import FlashcardList from './components/FlashcardList';
-import './index.css';
-import axios from 'axios';
+import './styles.css';
 
 export default function Index() {
-  const [flashcards, setFlashcards] = useState([]);
-  useEffect(() => {
-    axios.get('https://opentdb.com/api.php?amount=15').then((res) => {
-      const data = res.data.results.map((row, index) => {
-        // console.log(index+'-'+Date.now())
-        const id = `${index}-${Date.now()}`;
-        const options = [
-          ...row.incorrect_answers.map((a) => decodeString(a)),
-          row.correct_answer,
-        ];
-        return {
-          id,
-          question: decodeString(row.question),
-          answer: row.correct_answer,
-          options: options.sort(() => Math.random()-0.5 ),
-        };
-      });
-      setFlashcards(data);
-      // Math.random() 產生 0-1的小數, -0.5 就會有一半是正,一半是負
-      console.log(Math.random()-0.5);
-    });
-  }, []);
-
-  // 將一些內容為 html 符號轉成純文字
-  function decodeString(str) {
-    const textArea = document.createElement('textarea');
-    textArea.innerHTML = str;
-    return textArea.value;
-  }
+  const [flip, setFlip] = useState(false);
 
   return (
     <Container>
-      <FlashcardList flashcards={flashcards} />
+      {/* className='card flip' 在 css 中設定 .card.flip */}
+      {/* 動態改變樣式 */}
+      <div
+        className={`card ${flip ? 'flip' : ''}`}
+        onClick={() => {
+          setFlip(!flip);
+        }}
+      >
+        <div className="front">鳥</div>
+        <div className="back">Bird</div>
+      </div>
     </Container>
   );
 }
