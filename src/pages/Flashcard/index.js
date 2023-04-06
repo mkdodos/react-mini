1e632517d9588b2e496e4a7db0cc232972f9ecb8
mk <mkdodos@gmail.com>
2023-04-06 18:08:25 +0800

    firebase

--- src/pages/Flashcard/index.js
+++ src/pages/Flashcard/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Button, Container, Item } from 'semantic-ui-react';
+import { Button, Container, Input } from 'semantic-ui-react';
 import './styles.css';
 // import data from './data.json';
 import MycardList from './components/MycardList';
@@ -9,10 +9,17 @@ import { db } from '../../utils/firebase';
 export default function Index() {
   const [rows, setRows] = useState([]);
 
-  const [item, setItem] = useState({ch:'',en:''});
+  const [item, setItem] = useState({ ch: '', en: '' });
+
+  const [search, setSearch] = useState('');
 
   useEffect(() => {
-    db.collection('words').onSnapshot((snapshot) => {
+    if (search != '') {
+    } else {
+      
+    }
+
+    db.collection('words').limit(2).orderBy('createdAt','desc').onSnapshot((snapshot) => {
       const data = snapshot.docs.map((doc) => {
         return { ...doc.data(), id: doc.id };
       });
@@ -24,22 +31,54 @@ export default function Index() {
   }, []);
 
   function handleClick() {
-    db.collection('words').add(item).then(()=>setItem({en:'',ch:''}));
+    db.collection('words')
+      .add({ ...item, createdAt: Date.now() })
+      .then(() => setItem({ en: '', ch: '' }));
+  }
+
+  function handleSearch() {
+    db.collection('words')
+      // .where('en', '==', search)      
+      .where('en', '>=', search)
+      .where('en', '<=', search + '\uf8ff')
+      .get()
+      .then((snapshot) => {
+        const data = snapshot.docs.map((doc) => {
+          return { ...doc.data(), id: doc.id };
+        });
+        setRows(data);
+        console.log(snapshot.size);
+      });
   }
 
   function handleChange(e) {
     // console.log(e.target.name)
-    setItem({...item,[e.target.name]:e.target.value})
+    setItem({ ...item, [e.target.name]: e.target.value });
   }
 
   // console.log(rows);
 
   return (
     <Container>
-      {/* <Button onClick={handleClick}>新增</Button> */}
-      <input className='my-input' placeholder='guide' name="en" value={item.en} onChange={handleChange} />
-      <input className='my-input' placeholder='指導' name="ch" value={item.ch} onChange={handleChange} />
-      <button className='my-button' onClick={handleClick}>新增</button>
+      <Input value={search} onChange={(e) => setSearch(e.target.value)} />
+      <Button onClick={handleSearch}>搜尋</Button>
+      <input
+        className="my-input"
+        placeholder="guide"
+        name="en"
+        value={item.en}
+        onChange={handleChange}
+      />
+      <input
+        className="my-input"
+        placeholder="指導"
+        name="ch"
+        value={item.ch}
+        onChange={handleChange}
+      />
+      <button className="my-button" onClick={handleClick}>
+        新增
+      </button>
       <MycardList data={rows} />
       {/* className='card flip' 在 css 中設定 .card.flip */}
       {/* 動態改變樣式 */}
