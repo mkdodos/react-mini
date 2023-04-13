ac66c47068661d92fe849fddd6f0cfa182f6059b
mk <mkdodos@gmail.com>
2023-04-13 15:21:46 +0800

    trans form

--- src/pages/Posts/TransList.js
+++ src/pages/Posts/TransList.js
@@ -1,12 +1,42 @@
 import React from 'react';
-import { Input } from 'semantic-ui-react';
+import { Card, Input, Icon, List } from 'semantic-ui-react';
 import Trans from './Trans';
 import './index.css';
 
 export default function TransList({ rows }) {
   return (
     <div className="trans">
-      <div className="acc">
+      <Card>
+        {/* <Card.Content>
+          <Card.Header>Matthew</Card.Header>
+          <Card.Meta>
+            <span className="date">Joined in 2015</span>
+          </Card.Meta>
+        </Card.Content> */}
+
+        <Card.Content>
+          <List>
+            <List.Item>
+              <Trans rows={rows} />
+            </List.Item>
+            <List.Item>
+              <Input fluid placeholder="$ 轉帳金額" />
+            </List.Item>
+            <List.Item>
+              <Trans rows={rows} />
+            </List.Item>
+          </List>
+
+          {/* <Trans rows={rows} /> */}
+          {/* <Trans rows={rows} /> */}
+        </Card.Content>
+      </Card>
+      {/* <div className="card">
+        <Trans rows={rows} />
+        <Input placeholder="$ 轉帳金額" />
+        <Trans rows={rows} />
+      </div> */}
+      {/* <div className="acc">
         <div>
           <Trans rows={rows} />
         </div>
@@ -16,7 +46,7 @@ export default function TransList({ rows }) {
         <div>
           <Trans rows={rows} />
         </div>
-      </div>
+      </div> */}
     </div>
   );
 }
